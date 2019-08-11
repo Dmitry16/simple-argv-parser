ad5b6a9997d211a31de2e51d363fce4256d5ff53
Dmitry16 <europartner@inbox.ru>
2019-08-11 12:02:49 +0300

    ind+1_bug

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const obj = {};
-const argv = process.argv.slice(2).forEach((key, ind, self) => {
+process.argv.slice(2).forEach((key, ind, self) => {
   if (key.startsWith('-')) {
-    if (self[ind+1].startsWith('-')) {
+    if (self[ind+1] && self[ind+1].startsWith('-')) {
       obj[key] = null;
     } else {
       obj[key] = self[ind+1];
     }
   }
 });
-
-// console.log('process.argv:::', obj);
 
 module.exports = obj;
