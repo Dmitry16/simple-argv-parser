b4c678b7d683da69e24c04aaa60375bd34c8ef32
Dmitry16 <europartner@inbox.ru>
2019-08-13 18:53:01 +0300

    refactoring_to_reduce

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const obj = {};
-process.argv.slice(2).forEach((key, ind, self) => {
+const obj = process.argv.slice(2).reduce((acc, key, ind, self) => {
   if (key.startsWith('-')) {
     if (self[ind+1] && self[ind+1].startsWith('-')) {
-      obj[key] = null;
+      acc[key] = null;
     } else if (self[ind+1]) {
-      obj[key] = self[ind+1];
+      acc[key] = self[ind + 1];
     } else if (!self[ind+1]) {
-      obj[key] = null;
+      acc[key] = null;
     }
   }
-});
+  return acc;
+}, {});
 
 module.exports = obj;
