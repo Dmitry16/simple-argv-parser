360c2fc1774f2b60da8e4e420490e73e21c08a71
Dmitry16 <europartner@inbox.ru>
2019-08-21 09:40:16 +0300

    unit_tests

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const obj = process.argv.slice(2).reduce((acc, key, ind, self) => {
-  if (key.startsWith('-')) {
-    if (self[ind+1] && self[ind+1].startsWith('-')) {
-      acc[key] = null;
-    } else if (self[ind+1]) {
-      acc[key] = self[ind + 1];
-    } else if (!self[ind+1]) {
-      acc[key] = null;
-    }
-  }
-  return acc;
-}, {});
+const obj = parseArgs(process.argv);
 
-module.exports = obj;
+function parseArgs(arrArgs) {
+  return arrArgs.slice(2).reduce((acc, key, ind, self) => {
+    if (key.startsWith('-')) {
+      if (self[ind+1] && self[ind+1].startsWith('-')) {
+        acc[key] = null;
+      } else if (self[ind+1]) {
+        acc[key] = self[ind + 1];
+      } else if (!self[ind+1]) {
+        acc[key] = null;
+      }
+    }
+    return acc;
+  }, {});
+}
+
+console.log('obj:::', obj);
+
+exports.obj = obj;
+exports.parseArgs = parseArgs;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
-describe('it runs', () => {
-  it('works!!!', () => {
+let assert = require('assert');
+const { parseArgs } = require('..');
 
+describe('it parses arguments', () => {
+  it('returns an object with correctly parsed args', () => {
+    const args = [1, 2, '-a', 'b', 'c' , '-d'];
+    assert.deepEqual(parseArgs(args), {'-a': 'b', '-d': null});
   });
 });
